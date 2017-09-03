e78058b24bac8029c61c660b91a75e5aa5064602
Kamil P <aaaa@aaaa.aa>
2017-09-03 13:45:46 +0200

    zadanie 2 zrobione

--- cwieczenia/Program.cs
+++ cwieczenia/Program.cs
@@ -10,7 +10,20 @@ namespace cwieczenia
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj początek zakresu losowanie liczb: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj koniec zakresu losowania liczb: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+            int firstRnd = rnd.Next(firstNumber, secondNumber);
+            int secondRnd = rnd.Next(firstNumber, secondNumber);
+            int sum = firstRnd + secondRnd;
+            Console.WriteLine("Pierwsza wylosowana liczba to: " + firstRnd + ", druga wylosowana liczba: " 
+                + secondRnd + ". Suma tych liczb to: " + sum);
+            Console.ReadLine();
 
         }
     }
 }
+// Metoda do sumowania dwóch losowych liczb.Zakres losowanie musi zostać określony przez użytkownika.  
